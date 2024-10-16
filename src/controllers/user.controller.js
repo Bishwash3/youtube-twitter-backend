e0e966c0bf602bdbbf6c11b0bcc92a8b781c3477
Bishwash3 <bhusalbiswash2.com>
2024-10-16 10:41:15 +0545

    fixed getWatchHistory piepline and added all models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -437,14 +437,14 @@ const getWatchHistory = asyncHandler(async (req,res) => {
         },
         {
             $lookup: {
-                from: "video",
+                from: "videos",
                 localField: "watchHistory",
                 foreignField: "_id",
                 as: "watchHistory",
                 pipeline: [
                     {
                         $lookup: {
-                            from: "user",
+                            from: "users",
                             localField: "owner",
                             foreignField: "_id",
                             as: "owner",
