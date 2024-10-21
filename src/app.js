3000ddcb1e67c5aed50967adef226df24a32aae8
Bishwash3 <bhusalbiswash2.com>
2024-10-21 08:12:30 +0545

    typos fixed in app.js

--- src/app.js
+++ src/app.js
@@ -28,8 +28,8 @@ import healthRouter from './routes/healthcheck.routes.js'
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/likes", likeRouter)
 app.use("/api/v1/comments", commentRouter)
-app.use("api/v1/dashbord", dashbordRouter)
-app.use("api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashbord", dashbordRouter)
+app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/healthcheck", healthRouter)
 
 
